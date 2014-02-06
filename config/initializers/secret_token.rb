d85370fc98cfd252fd465173d262bcfb7ce9cddc
Brett Shollenberger <brett.shollenberger@gmail.com>
2014-02-05 19:40:26 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SchedulingOptimizer::Application.config.secret_key_base = 'd33422823f792d6ffd7b963a90a519af815edc3e030686a1664d466b0dcd9234627257669067ea714df3ef28a2dd550cb9434ff0d92bde390d73ffee2a646afc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SchedulingOptimizer::Application.config.session_store :cookie_store, key: '_scheduling_optimizer_session'
